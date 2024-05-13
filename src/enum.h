6521a009d1d1923e87b524c75f6ba1e2c6552c8c
kr0ner <dkroner87@gmail.com>
2024-05-13 22:27:20 +0200

    Debloat property and type mappings
    It does not make sense to carry along more than 3k unused
    properties. This commit gets rid of the unused ones and should
    not affect the already implemented functionality. If we need
    some of them back, we can enable them one by one.

--- src/enum.h
+++ src/enum.h
@@ -1,29 +1,34 @@
 #if !defined(ENUM_H)
-#define TYPE_H
+#define ENUM_H
 
-#define DECLARE_ENUM_WITH_TYPE(E, T, ...)                                                \
-    enum class E : T                                                                     \
-    {                                                                                    \
-        __VA_ARGS__                                                                      \
-    };                                                                                   \
-    std::unordered_map<T, std::string_view> E##MapName(generateEnumMap<T>(#__VA_ARGS__));\
-    std::ostream &operator<<(std::ostream &os, const E enumTmp)                          \
-    {                                                                                    \
-        os << static_cast<T>(enumTmp);                                                   \
-        return os;                                                                       \
-    }                                                                                    \
-    std::string_view getName(E enumTmp) { return E##MapName[static_cast<T>(enumTmp)]; }
+#include <cstdint>
+#include <ostream>
+#include <string_view>
+#include <unordered_map>
+
+// clang-format off
+#define DECLARE_ENUM_WITH_TYPE(E, T, ...)                                                       \
+    enum E : T                                                                                  \
+    {                                                                                           \
+        __VA_ARGS__                                                                             \
+    };                                                                                          \
+    static std::unordered_map<T, std::string_view> E##MapName(generateEnumMap<T>(#__VA_ARGS__));\
+    inline std::ostream &operator<<(std::ostream &os, const E enumTmp)                          \
+    {                                                                                           \
+        os << static_cast<T>(enumTmp);                                                          \
+        return os;                                                                              \
+    }                                                                                           \
+    inline std::string_view getName(E enumTmp) { return E##MapName[static_cast<T>(enumTmp)]; }
+// clang-format on
 
 template <typename T>
-std::unordered_map<T, std::string_view> generateEnumMap(std::string_view stringView)
-{
+std::unordered_map<T, std::string_view> generateEnumMap(std::string_view stringView) {
     std::unordered_map<T, std::string_view> retMap;
 
     std::size_t begin{0U};
     std::size_t end{stringView.size()};
-    constexpr auto minSize{3U}; // a=b
-    while(stringView.size() >= minSize)
-    {
+    constexpr auto minSize{3U};  // a=b
+    while (stringView.size() >= minSize) {
         // normalize stringView
         stringView.remove_prefix(stringView.find_first_not_of(" ,"));
 
@@ -34,19 +39,18 @@ std::unordered_map<T, std::string_view> generateEnumMap(std::string_view stringV
 
         // remove the element from the stringView
         stringView.remove_prefix(element.size());
-        
+
         // create a subview for the name
         std::string_view name = element;
         name.remove_suffix(element.size() - element.find('='));
-        name.remove_prefix(1U); // remove the leading 'k'
+        name.remove_prefix(1U);  // remove the leading 'k'
 
         // create a subview for the value
         std::string_view value = element;
         value.remove_prefix(1U + element.find('='));
 
-        retMap.insert({static_cast<T>(std::strtoul(value.data(), nullptr, 0)),name});
+        retMap.insert({static_cast<T>(std::strtoul(value.data(), nullptr, 0)), name});
     }
-
     return retMap;
 }
 
