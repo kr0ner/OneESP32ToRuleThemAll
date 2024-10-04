580f799f67d346bea1058e1dba31d3260b433329
kr0ner <dkroner87@gmail.com>
2024-10-04 19:33:01 +0200

    Calculate read/write ids based on CAN id
    As proposed by @croessi

--- src/communication.h
+++ src/communication.h
@@ -14,23 +14,17 @@ using CANID = std::uint16_t;
 struct CanMember {
     CANID CanId;
     std::string name;
-    std::uint8_t ReadId[2U];
-    std::uint8_t WriteId[2U];
-    std::uint8_t ConfirmationId[2U];
     bool operator<(const CanMember& other) const { return CanId < other.CanId; }
-    std::uint16_t getWriteId() const { return (WriteId[1U] | (WriteId[0U] << 8U)); }
-    std::uint16_t getReadId() const { return (ReadId[1U] | (ReadId[0U] << 8U)); }
+    std::uint16_t getWriteId() const { return (((CanId & 0x7c0) << 5U) + (CanId & 0x3f)); }
+    std::uint16_t getReadId() const { return getWriteId() | 0x100; }
+    std::uint16_t getResponseId() const { return getWriteId() | 0x200; }
 };
 
-// clang-format off
-//  Name                          CanId  Name          ReadId        WriteId       ConfirmationID
-static const CanMember ESPClient {0x6a2, "ESPClient", {0x00, 0x00}, {0x00, 0x00}, {0xE2, 0x00}};
-static const CanMember Anfrage   {0x6a1, "Anfrage",   {0x00, 0x00}, {0xd2, 0x21}, {0x00, 0x00}};
-static const CanMember Kessel    {0x180, "Kessel",    {0x31, 0x00}, {0x30, 0x00}, {0x00, 0x00}};
-static const CanMember HK1       {0x301, "HK1",       {0x61, 0x01}, {0x60, 0x01}, {0x00, 0x00}};
-static const CanMember HK2       {0x302, "HK2",       {0x61, 0x02}, {0x60, 0x02}, {0x00, 0x00}};
-static const CanMember FEK       {0x401, "FEK",       {0x61, 0x01}, {0x60, 0x01}, {0x00, 0x00}};
-// clang-format on
+static const CanMember ESPClient{0x6a2, "ESPClient"};
+static const CanMember Anfrage{0x6a1, "Anfrage"};
+static const CanMember Kessel{0x180, "Kessel"};
+static const CanMember HK1{0x301, "HK1"};
+static const CanMember HK2{0x302, "HK2"};
 
 using Request = std::pair<const CanMember, const Property>;
 static std::queue<Request> request_queue;
@@ -40,7 +34,7 @@ static std::queue<Request> request_queue;
  */
 bool isRequest(const std::vector<std::uint8_t>& msg) {
     const auto id{msg[1U] | (msg[0U] << 8U)};
-    const std::vector<CanMember> members = {ESPClient, Anfrage, Kessel, HK1, HK2, FEK};
+    const std::vector<CanMember> members = {ESPClient, Anfrage, Kessel, HK1, HK2};
     const auto it =
         std::find_if(members.cbegin(), members.cend(), [id](const auto& member) { return member.getWriteId() == id; });
     return it != members.cend();
@@ -93,8 +87,8 @@ std::pair<Property, SimpleVariant> processCanMessage(const std::vector<std::uint
 
 void requestData(const CanMember& member, const Property& property) {
     const auto use_extended_id{false};  //No use of extended ID
-    const std::uint8_t IdByte1{member.ReadId[0U]};
-    const std::uint8_t IdByte2{member.ReadId[1U]};
+    const std::uint8_t IdByte1{static_cast<std::uint8_t>((member.getReadId() >> 8U) & 0xff)};
+    const std::uint8_t IdByte2{static_cast<std::uint8_t>(member.getReadId() & 0xff)};
     const std::uint8_t IndexByte1{static_cast<std::uint8_t>((property >> 8U) & 0xff)};
     const std::uint8_t IndexByte2{static_cast<std::uint8_t>(property & 0xff)};
     std::vector<std::uint8_t> data;
@@ -112,8 +106,8 @@ void requestData(const CanMember& member, const Property& property) {
  */
 void sendData(const CanMember& member, const Property property, const std::uint16_t value) {
     const auto use_extended_id{false};  //No use of extended ID
-    const std::uint8_t IdByte1{member.WriteId[0U]};
-    const std::uint8_t IdByte2{member.WriteId[1U]};
+    const std::uint8_t IdByte1{static_cast<std::uint8_t>((member.getWriteId() >> 8U) & 0xff)};
+    const std::uint8_t IdByte2{static_cast<std::uint8_t>(member.getWriteId() & 0xff)};
     const std::uint8_t IndexByte1{static_cast<std::uint8_t>((property >> 8U) & 0xff)};
     const std::uint8_t IndexByte2{static_cast<std::uint8_t>(property & 0xff)};
     const std::uint8_t ValueByte1{static_cast<std::uint8_t>((value >> 8U) & 0xff)};
