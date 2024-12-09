2548cf1d066037aed8f2fd03ccf3518c18e053df
kr0ner <dkroner87@gmail.com>
2024-12-09 10:12:25 +0100

    Add function to search CANMember by CANId

--- src/communication.h
+++ src/communication.h
@@ -9,13 +9,13 @@
 #include "property.h"
 #include "type.h"
 
-using CANID = std::uint16_t;
+using CANId = std::uint16_t;
 
 struct CanMember {
-    CANID CanId;
+    CANId canId;
     std::string name;
-    bool operator<(const CanMember& other) const { return CanId < other.CanId; }
-    std::uint16_t getWriteId() const { return (((CanId & 0x7c0) << 5U) + (CanId & 0x3f)); }
+    bool operator<(const CanMember& other) const { return canId < other.canId; }
+    std::uint16_t getWriteId() const { return (((canId & 0x7c0) << 5U) + (canId & 0x3f)); }
     std::uint16_t getReadId() const { return getWriteId() | 0x100; }
     std::uint16_t getResponseId() const { return getWriteId() | 0x200; }
 };
@@ -26,18 +26,45 @@ static const CanMember Kessel{0x180, "Kessel"};
 static const CanMember HK1{0x301, "HK1"};
 static const CanMember HK2{0x302, "HK2"};
 
+static const std::vector<std::reference_wrapper<const CanMember>> canMembers{Anfrage, Kessel, HK1, HK2};
+
 using Request = std::pair<const CanMember, const Property>;
 static std::queue<Request> request_queue;
+
+/**
+ * @brief Tries to find the CANMember with the given CANId.
+ *
+ * @param The CANId to look up.
+ * @return A reference to the CANMember wrapped in an optional.
+ */
+std::optional<std::reference_wrapper<const CanMember>> getCanMemberByCanId(CANId canId) {
+    const auto it = std::find_if(canMembers.cbegin(), canMembers.cend(),
+                                 [canId](const auto& member) { return member.get().canId == canId; });
+    if (it != canMembers.cend()) {
+        return it->get();
+    }
+    return std::nullopt;
+}
 
 /**
  * @brief Checks if the message is a request, sent by another CAN participant.
  */
 bool isRequest(const std::vector<std::uint8_t>& msg) {
     const auto id{msg[1U] | (msg[0U] << 8U)};
-    const std::vector<CanMember> members = {ESPClient, Anfrage, Kessel, HK1, HK2};
-    const auto it =
-        std::find_if(members.cbegin(), members.cend(), [id](const auto& member) { return member.getWriteId() == id; });
-    return it != members.cend();
+    const auto it = std::find_if(canMembers.cbegin(), canMembers.cend(), [id](const auto& member) {
+        return member.get().getReadId() == id || member.get().getWriteId() == id;
+    });
+    return it != canMembers.cend();
+}
+
+/**
+ * @brief Checks if the message is a response to a previously sent request. Responses contain the actual sensor value.
+ */
+bool isResponse(const std::vector<std::uint8_t>& msg) {
+    const auto id{msg[1U] | (msg[0U] << 8U)};
+    const auto it = std::find_if(canMembers.cbegin(), canMembers.cend(),
+                                 [id](const auto& member) { return member.get().getResponseId() == id; });
+    return it != canMembers.cend();
 }
 
 /**
@@ -96,7 +123,7 @@ void requestData(const CanMember& member, const Property& property) {
 
     data.insert(data.end(), {IdByte1, IdByte2, 0xfa, IndexByte1, IndexByte2, 0x00, 0x00});
 
-    id(wp_can).send_data(ESPClient.CanId, use_extended_id, data);
+    id(wp_can).send_data(ESPClient.canId, use_extended_id, data);
 }
 
 /**
@@ -117,7 +144,7 @@ void sendData(const CanMember& member, const Property property, const std::uint1
 
     data.insert(data.end(), {IdByte1, IdByte2, 0xfa, IndexByte1, IndexByte2, ValueByte1, ValueByte2});
 
-    id(wp_can).send_data(ESPClient.CanId, use_extended_id, data);
+    id(wp_can).send_data(ESPClient.canId, use_extended_id, data);
 }
 
 #endif
