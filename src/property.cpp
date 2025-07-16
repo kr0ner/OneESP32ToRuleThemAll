4e142fef29669e044936640c3322771eb953eafc
kr0ner <dkroner87@gmail.com>
2025-07-16 22:04:45 +0200

    workaround for namespace clash

--- src/property.cpp
+++ src/property.cpp
@@ -1,6 +1,7 @@
 #include "property.h"
 #include <unordered_map>
 
+namespace oe32trta {
 namespace detail {
 Property Mapper::getProperty(const std::uint16_t id) {
     if (auto it = map.find(id); it != map.end()) {
@@ -13,3 +14,4 @@ Property Property::getProperty(const std::uint16_t id) {
     return Mapper().getProperty(id);
 }
 }  // namespace detail
+}  // namespace oe32trta
