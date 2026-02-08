76009babbd8a3a316b5328d8e76691fa8857eff1
kr0ner <dkroner87@gmail.com>
2026-02-08 22:44:16 +0100

    Drop SNTP time and use recommended Home Assitant time platform

--- src/util.h
+++ src/util.h
@@ -8,21 +8,21 @@
 #include "type.h"
 
 /**
- * @brief Synchronizes the time of the heat pump to the one received via sntp.
+ * @brief Synchronizes the time of the heat pump to the one received via Home Assistant time platform.
  */
 void syncTime() {
-    const auto time = id(sntp_time).now();
+    const auto time = id(homeassistant_time).now();
     if (!time.is_valid()) {
-        ESP_LOGE("TIMESYNC", "Cannot sync time. SNTP time is invalid.");
+        ESP_LOGE("TIMESYNC", "Cannot sync time. Home Assistant time is invalid.");
         return;
     }
     ESP_LOGI("TIMESYNC", "Synchronizing heat pump time to %d.%d.%d %d:%d", time.day_of_month, time.month, time.year,
              time.hour, time.minute);
-    queueTransmission(Kessel, Property::kJAHR, toggleEndianness(time.year - 2000U));
-    queueTransmission(Kessel, Property::kMONAT, toggleEndianness(time.month));
-    queueTransmission(Kessel, Property::kTAG, toggleEndianness(time.day_of_month));
-    queueTransmission(Kessel, Property::kSTUNDE, toggleEndianness(time.hour));
-    queueTransmission(Kessel, Property::kMINUTE, toggleEndianness(time.minute));
+    sendData(Kessel, Property::kJAHR, toggleEndianness(time.year - 2000U));
+    sendData(Kessel, Property::kMONAT, toggleEndianness(time.month));
+    sendData(Kessel, Property::kTAG, toggleEndianness(time.day_of_month));
+    sendData(Kessel, Property::kSTUNDE, toggleEndianness(time.hour));
+    sendData(Kessel, Property::kMINUTE, toggleEndianness(time.minute));
 }
 
 #endif
